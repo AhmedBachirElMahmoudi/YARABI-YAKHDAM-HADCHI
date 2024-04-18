6f6051aea89b290e4f0f382ec2295a693ac5bad4
ahmedbachir <2ahmedbachir0@gmail.com>
2024-04-18 07:50:32 +0100

    First commit

---
+++ example-app/app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string'
+        ]);
+
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        return response()->json(['message' => 'User created successfully'], 201);
+
+    }
+
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        if(!Auth::attempt($data)){
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60*24);
+
+        return response()->json(['message' => 'Logged in successfully'], 200)->withCookie($cookie);
+
+
+    }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+
+
+    public function user(Request $request)
+    {
+        return Auth::user();
+    }
+
+
+}
